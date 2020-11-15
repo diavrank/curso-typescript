266ffd196fe6986529d29b5d2ed87b6f92a160a3
Iván Cabrera <hec_ivan_150@hotmail.com>
2020-11-15 17:59:58 -0600

    Feature: Se agregan ejemplos de genéricos

---
+++ app/genericos.ts
@@ -0,0 +1,36 @@
+console.log('Genéricos')
+class Lista<T> {
+    elementos: Array<T>;
+
+    constructor() {
+        this.elementos = [];
+    }
+
+    agregar(elemento: T) {
+        this.elementos.push(elemento);
+    }
+
+    sumar(): T {
+        let resultado: any;
+        this.elementos.forEach(a => {
+            if (!resultado) {
+                resultado = a;
+            } else {
+                resultado += a;
+            }
+        });
+        return resultado;
+    }
+}
+
+let misNumeros=new Lista<number>();
+misNumeros.agregar(10);
+misNumeros.agregar(5);
+
+console.log('misNumeros: ',misNumeros)
+
+let misCadenas=new Lista<string>();
+misCadenas.agregar('hola ');
+misCadenas.agregar('mundo');
+
+console.log('misCadenas: ',misCadenas);
