adb72f037fad73cc4d61704ba397c002e5d60ef6
Iván Cabrera <hec_ivan_150@hotmail.com>
2020-11-15 18:30:48 -0600

    Feature: Se agrega función de ejemplo para types

--- app/types.ts
+++ app/types.ts
@@ -2,14 +2,20 @@ type Heroe = {
     nombre: string,
     edad: number,
     poderes: string[],
-    getNombre: () => string
 }
 
 let flash: Heroe = {
     nombre: 'Flash',
     edad: 24,
-    poderes: ['Corre rapido'],
-    getNombre() {
-        return this.nombre;
-    }
+    poderes: ['Corre rapido']
 }
+
+const getInfoOfHero: Function = (hero: Heroe) => {
+    return JSON.stringify(hero);
+}
+
+console.log('datos del heroe: ', getInfoOfHero({
+    nombre: 'Batman',
+    poderes: ['fuerza'],
+    superpoder: 'inteligencia'
+}));
